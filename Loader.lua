1051d5cc8361cb0f92f2e1dd2249979805117c00
Unc <135977564+Uncsypn@users.noreply.github.com>
2023-07-05 18:32:53 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,14 +1,14 @@
 local Ids = {
-    [{2866967438}] = "", -- Fishing Simulator
-    [{621129760}] = "", -- Kat
-    [{3587619225}] = "", -- Mega Easy Obby
+    [{2866967438}] = "https://raw.githubusercontent.com/Uncsypn/SVillyCollection/main/Fishing-Simulator.lua", -- Fishing Simulator
+    [{621129760}] = "https://raw.githubusercontent.com/Uncsypn/SVillyCollection/main/Kat.lua", -- Kat
+    [{3587619225}] = "https://raw.githubusercontent.com/Uncsypn/SVillyCollection/main/Mega-Easy-Obby.lua", -- Mega Easy Obby
     [{3851622790}] = "https://raw.githubusercontent.com/Uncsypn/SVillyCollection/main/Break-In_Story.lua", -- Break In (Story)
     [{3587619225}] = "https://raw.githubusercontent.com/Uncsypn/SVillyCollection/main/The-Rake_Remastered.lua", -- The Rake (Remastered)
 }
 
-for ids, url in next, Ids do
-    if table.find(ids, game.PlaceId) then
-        loadstring(game:HttpGet(url))()
+for sex, mommy in next, Ids do
+    if table.find(sex, game.PlaceId) then
+        loadstring(game:HttpGet(mommy))()
         break
     end
 end
