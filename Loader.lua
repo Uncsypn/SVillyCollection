0efe0425df2ffba21fc51f71fb7d45df827e3e95
Unc <135977564+Uncsypn@users.noreply.github.com>
2023-07-05 17:55:49 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,8 +2,8 @@ local Ids = {
     [{2866967438}] = "", -- Fishing Simulator
     [{621129760}] = "", -- Kat
     [{3587619225}] = "", -- Mega Easy Obby
-    [{3851622790}] = "", -- Break In (Story)
-    [{3587619225}] = "", -- The Rake (Remastered)
+    [{3851622790}] = "https://raw.githubusercontent.com/Uncsypn/SVillyCollection/main/Break-In_Story.lua", -- Break In (Story)
+    [{3587619225}] = "https://raw.githubusercontent.com/Uncsypn/SVillyCollection/main/The-Rake_Remastered.lua", -- The Rake (Remastered)
 }
 
 for ids, url in next, Ids do
